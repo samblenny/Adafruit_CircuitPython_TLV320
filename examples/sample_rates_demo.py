832eac732d09fcf917f91384810e08c6ac904fa7
sam blenny <68084116+samblenny@users.noreply.github.com>
2025-12-22 11:11:57 +0000

    try to make ruff happy

--- examples/sample_rates_demo.py
+++ examples/sample_rates_demo.py
@@ -3,21 +3,21 @@
 #
 # WAV file beep demo for Fruit Jam (adjust pins as needed for other boards)
 #
-from audiobusio import I2SOut
+import time
+
 import audiocore
-from board import (
-    I2C, I2S_BCLK, I2S_DIN, I2S_MCLK, I2S_WS, PERIPH_RESET
-)
+from audiobusio import I2SOut
+from board import I2C, I2S_BCLK, I2S_DIN, I2S_MCLK, I2S_WS, PERIPH_RESET
 from digitalio import DigitalInOut, Direction, Pull
 from pwmio import PWMOut
-import time
+
 from adafruit_tlv320 import TLV320DAC3100
 
 # To try different sample rates, change which one of these is uncommented
-#SAMPLE_RATE = 48000
-#SAMPLE_RATE = 44100
-#SAMPLE_RATE = 22050
-#SAMPLE_RATE = 11025
+# SAMPLE_RATE = 48000
+# SAMPLE_RATE = 44100
+# SAMPLE_RATE = 22050
+# SAMPLE_RATE = 11025
 SAMPLE_RATE = 8000
 
 BUFFER_SIZE = 512
@@ -37,17 +37,19 @@ mclk_out = PWMOut(I2S_MCLK, frequency=5_000_000, duty_cycle=2**15)
 dac.configure_clocks(sample_rate=SAMPLE_RATE, bit_depth=16, mclk_freq=5_000_000)
 dac.speaker_output = False
 dac.headphone_output = True
-dac.headphone_volume = -6    # CAUTION! Line level. Too loud for headphones!
+dac.headphone_volume = -6  # CAUTION! Line level. Too loud for headphones!
 audio = I2SOut(bit_clock=I2S_BCLK, word_select=I2S_WS, data=I2S_DIN)
 
 # Load wav file with 650 Hz beep at the specified sample rate
-sample = audiocore.WaveFile({
-    8000: "sinewave_8kHz.wav",
-    11025: "sinewave_11kHz.wav",
-    22050: "sinewave_22kHz.wav",
-    44100: "sinewave_44kHz.wav",
-    48000: "sinewave_48kHz.wav",
-}[SAMPLE_RATE])
+sample = audiocore.WaveFile(
+    {
+        8000: "sinewave_8kHz.wav",
+        11025: "sinewave_11kHz.wav",
+        22050: "sinewave_22kHz.wav",
+        44100: "sinewave_44kHz.wav",
+        48000: "sinewave_48kHz.wav",
+    }[SAMPLE_RATE]
+)
 
 # Send wav file beeps on the Fruit Jam DAC
 print(f"Beeping at sample rate {SAMPLE_RATE} Hz...")
